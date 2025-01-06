da4babe372a81500176963325397c799929a6978
Aurélien C. <a.cretaine@outlook.com>
2025-01-06 18:18:26 +0100

    Initialize DbContext with Postgres

---
+++ src/RecettesFamille/Data/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecettesFamille.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                var postgresCs = "Host=172.26.0.29;Port=5442;Database=recettesfamilledb;Username=pguser;Password=PGUserPwd";
+                options.UseNpgsql(postgresCs);
+            }, ServiceLifetime.Transient);
+
+            return services;
+        }
+    }
+}
--- src/RecettesFamille/Program.cs
+++ src/RecettesFamille/Program.cs
@@ -33,9 +33,8 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddCustomDbContext();
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
