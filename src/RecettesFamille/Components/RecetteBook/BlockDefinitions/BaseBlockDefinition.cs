3d4313a41a9103c5f9f2c1216cbcdee43e286a8c
Aurélien C. <a.cretaine@outlook.com>
2025-01-11 21:14:15 +0100

    New implementation of Edit and Delete Block

--- src/RecettesFamille/Components/RecetteBook/BlockDefinitions/BaseBlockDefinition.cs
+++ src/RecettesFamille/Components/RecetteBook/BlockDefinitions/BaseBlockDefinition.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using RecettesFamille.Components.RecetteBook.BlockEditors;
 using RecettesFamille.Data;
 using RecettesFamille.Data.EntityModel.RecipeSubEntity;
 
@@ -10,38 +9,10 @@ public abstract class BaseBlockDefinition<TBlock> : ComponentBase where TBlock :
 {
     [Inject] IDialogService DialogService { get; set; }
     [Inject] ApplicationDbContext dbContext { get; set; }
+
     [Parameter] public TBlock Block { get; set; }
-    [Parameter] public Action BlockUpdated { get; set; }
+    [Parameter] public bool EditMode { get; set; }
 
-
-    public async Task OpenEditor()
-    {
-        DialogOptions dialogOptions = new() { MaxWidth = MaxWidth.Medium, FullWidth = true };
-        DialogResult result;
-        IDialogReference dialog = default!;
-
-        switch (Block)
-        {
-            case BlockImageEntity blockImage:
-                break;
-            case BlockIngredientListEntity blockIngredientList:
-                break;
-            case BlockInstructionEntity blockInstruction:
-                var parameters = new DialogParameters<InstructionEditor> { { x => x.blockInstructionEntity, blockInstruction } };
-                dialog = await DialogService.ShowAsync<InstructionEditor>("test", parameters, dialogOptions);
-                break;
-
-            default:
-                throw new InvalidOperationException($"Type de bloc non pris en charge : {typeof(TBlock).Name}");
-        }
-
-        result = await dialog.Result;
-
-        if (result.Canceled)
-            return;
-
-        dbContext.Update(Block);
-        await dbContext.SaveChangesAsync();
-    }
-
+    [Parameter] public Func<BlockBase, Task> OnBlockUpdated { get; set; }
+    [Parameter] public Func<BlockBase, Task> OnBlockDeleted { get; set; }
 }
