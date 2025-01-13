a0407ace410172185d3effe831571d873a2119c2
Aurélien C. <a.cretaine@outlook.com>
2025-01-13 19:17:13 +0100

    Change block management

--- src/RecettesFamille/Components/RecetteBook/BlockDefinitions/BaseBlockDefinition.cs
+++ src/RecettesFamille/Components/RecetteBook/BlockDefinitions/BaseBlockDefinition.cs
@@ -14,5 +14,9 @@ public abstract class BaseBlockDefinition<TBlock> : ComponentBase where TBlock :
     [Parameter] public bool EditMode { get; set; }
     [Parameter] public Func<BlockBase, Task> OnBlockUpdated { get; set; }
     [Parameter] public Func<BlockBase, Task> OnBlockDeleted { get; set; }
+    [Parameter] public Func<BlockBase, Task> MoveUp { get; set; }
+    [Parameter] public Func<BlockBase, Task> MoveDown { get; set; }
     [Parameter] public Func<Task> OnBlockHasChanged { get; set; }
+
+    
 }
