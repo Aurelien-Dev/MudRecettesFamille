c087de692bf5273fa31d1845a7fe3d9a0ed3c6ea
Aurélien C. <a.cretaine@outlook.com>
2025-02-22 22:08:37 +0100

    Increase max length for ingredient properties
    Updated the `Name` and `Quantity` properties in the
    `IngredientEntity` class to a maximum length of 100 characters.
    The `Name` property in the `BlockIngredientListEntity` class
    has also been updated to match. A new migration file
    `20250222210737_UpdateLengthOfAllIngredientsFields`
    has been created to apply these changes to the database schema,
    and the `ApplicationDbContextModelSnapshot.cs` file
    has been updated accordingly.

---
+++ src/RecettesFamille.Data/Migrations/20250222210737_UpdateLengthOfAllIngredientsFields.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecettesFamille.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLengthOfAllIngredientsFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Quantity",
+                table: "IngredientEntity",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "IngredientEntity",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "BlockIngredientListEntity",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Quantity",
+                table: "IngredientEntity",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "IngredientEntity",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "BlockIngredientListEntity",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
