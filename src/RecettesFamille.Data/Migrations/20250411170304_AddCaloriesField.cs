1a5f1b90ff9f16a60d50c87a5698e9dd974b26e8
Aurélien C. <a.cretaine@outlook.com>
2025-04-11 19:49:50 +0200

    Add calories fonctionnality

---
+++ src/RecettesFamille.Data/Migrations/20250411170304_AddCaloriesField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecettesFamille.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCaloriesField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Calories",
+                table: "BlockIngredientListEntity",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "BlockIngredientListEntity");
+        }
+    }
+}
